77f2d11a16e04b70973f8a38ca021d97ebd912bf
Nicolas-Antonio-Kumm-Hoger <159266658+Nicolas-Antonio-Kumm-Hoger@users.noreply.github.com>
2025-02-27 09:36:53 -0300

    Create LojaBOoMBOX.c

---
+++ LojaBOoMBOX.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    
+    int menu;
+    
+    printf("Escolha \n 1)Controle de estoque\n 2)Controle de vendas\n 3)Controle financeiro\n 4)SAIR\n");
+    scanf("%d", &menu);
+    
+    switch(menu){
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        default:
+            printf("OPÇÃO INVALIDA!");
+            break;
+    }
+    return 0;
+}
