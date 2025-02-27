f8c1aa118369753d9f5e0034bff74fe57c9a662e
eduardocheslackidurante <160148439+eduardocheslackidurante@users.noreply.github.com>
2025-02-27 08:40:48 -0300

    Create seloco.c

---
+++ seloco.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int e;
+    
+    printf("digite um numero ");
+    scanf("%d", &e);
+    
+    if(e<=100){
+        printf("vc é gay");
+    }else{
+        printf("SE É LOCO?");
+    }
+
+    return 0;
+}
